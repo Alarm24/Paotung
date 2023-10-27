ff29f43c1e5da2a69b77703fa890549cae52f30e
Alarm24 <alarmirl24@gmail.com>
2023-10-27 22:20:51 +0700

    Update restaurant.js

--- frontend/src/restaurant.js
+++ frontend/src/restaurant.js
@@ -1,12 +1,25 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 
 function Restaurant() {
-  axios.get("http://localhost:5050/restaurant").then((res) => {
-    const data = res;
-  });
+  const [restaurant, setRestaurant] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:5050/restaurant")
+      .then((res) => {
+        setRestaurant(res.data); // Set the restaurant data using the setter
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+      });
+  }, []);
   return (
     <>
-      <div></div>
+      <div>
+        {restaurant.map((item) => (
+          <div key={item.id}>{item.name}</div>
+        ))}
+      </div>
     </>
   );
 }
