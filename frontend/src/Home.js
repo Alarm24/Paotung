eeff93979fbf0874fcd193fdc03b748fec3eb761
Alarm24 <alarmirl24@gmail.com>
2023-10-27 21:36:14 +0700

    Update Home.js

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -2,6 +2,8 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+const mockData = { bulb: 1000 };
+
 function Home() {
   const [name, setName] = useState("");
   const navigate = useNavigate("");
@@ -22,6 +24,9 @@ function Home() {
   return (
     <div>
       <h1>Hi</h1>
+      <h1>{mockData.bulb}</h1>
+      <button>โรงอาหาร</button>
+      <button>สุ่ม</button>
     </div>
   );
 }
