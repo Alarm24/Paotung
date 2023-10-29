cf79a9f38ce464cb03ba90e8bbdedf1d5bbf97a3
Alarm24 <alarmirl24@gmail.com>
2023-10-29 13:41:38 +0700

    AddPaothoong

---
+++ frontend/src/setupProxy.js
@@ -0,0 +1,29 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use(
+    "/v1/auth/register",
+    createProxyMiddleware({
+      target: "https://paotooong.thinc.in.th",
+      changeOrigin: true,
+      secure: false,
+      onProxyReq: (proxyReq, req) => {
+        if (req.body) {
+          let bodyData = JSON.stringify(req.body);
+          proxyReq.setHeader("Content-Type", "application/json");
+          proxyReq.setHeader("Content-Length", Buffer.byteLength(bodyData));
+          proxyReq.write(bodyData);
+        }
+      },
+    })
+  );
+
+  app.use(
+    "/v1/auth/me",
+    createProxyMiddleware({
+      target: "https://paotooong.thinc.in.th",
+      changeOrigin: true,
+      secure: false,
+    })
+  );
+};
