23893bcdc420a945741faf0881fd81ed0e49c38a
Alarm24 <alarmirl24@gmail.com>
2023-10-29 11:21:39 +0700

    Update RestaurantDetail.js

--- frontend/src/RestaurantDetail.js
+++ frontend/src/RestaurantDetail.js
@@ -1,4 +1,7 @@
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import CheckSessionUser from "./CheckSessionUser";
 
 function RestaurantDetail() {
   const restaurant = useNavigate();
@@ -6,8 +9,27 @@ function RestaurantDetail() {
     restaurant("/restaurants");
   }
 
+  CheckSessionUser();
+  axios.defaults.withCredentials = true;
+  const [menu, setMenu] = useState([]);
+  useEffect(() => {
+    axios
+      .post("http://localhost:5050/menus", {
+        restaurant_name: "starbar",
+      })
+      .then((res) => {
+        setMenu(res.data); // Set the restaurant data using the setter
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+      });
+  }, []);
+
   return (
     <>
+      {menu.map((item) => (
+        <div key={item.id}>{item.menu_name}</div>
+      ))}
       <button onClick={handleClickRestaurant}>โรงอาหาร</button>
     </>
   );
